72be1811cffafb7f2b0bb83e04f25e88edc6328d
Justin KA <justinalexander101@gmail.com>
2023-05-03 21:44:16 -0400

    intial setup colors font

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["**/*.{html, js}", "**/**/*.{html, js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Josefin Sans', 'sans-serif'],
+    },
+    screens: {
+      'xs': '361px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        'primary-color-one': '#371d0f',
+        'primary-color-two': '#fff4de',
+        'secondary-color-one': '#2c1503',
+        'secondary-color-one': '#89471a'
+      }
+    },
+  },
+  plugins: [],
+}
+
